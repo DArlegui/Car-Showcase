75e0738c9168975570641890e11aa7930140ba93
daniel <arleguidaniel@gmail.com>
2023-10-31 14:11:22 +0900

    added components and img

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,32 @@
+'use client';
 import Image from 'next/image';
+import CustomButton from './CustomButton';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  const handleScroll = () => {};
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">Lorem ipsum dolor sit amet.</h1>
+        <p className="hero__subtitle">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+          distinctio.
+        </p>
+        <CustomButton
+          title="Explore"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
